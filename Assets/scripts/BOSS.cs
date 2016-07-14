b8cd23a78e3a77689c2d76e6e0195c05ef97c4cb
RobsonCN2015 <robsoncavalcante.es@gmail.com>
2016-07-14 19:01:48 -0300

    Alterações na fase teste - criação de animacao e movimentacao do BOSS

---
+++ Assets/scripts/BOSS.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BOSS : MonoBehaviour {
+	public float velocidade;
+	public bool direcao;
+	public float duracaoDirecao;
+	private float tempoNaDirecao;
+	private Animator animator;
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.transform.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var distancia = (player.transform.position.x - transform.position.x);
+		if (distancia > 0) {
+			transform.eulerAngles = new Vector2(0, 0);
+		} else {
+			transform.eulerAngles = new Vector2(0, 180);
+		}
+		transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+
+		if (distancia > 10){
+			animator.SetBool("Atack", true);
+		}
+	}
+}
